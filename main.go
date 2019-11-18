a32a842ff34850bbd16544fc5003de04a1146fbe
JW Kim <dasider41@gmail.com>
2019-11-19 12:34:34 +1300

    Add cron schedule and gin web server for API

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/dasider41/cfootsell/email"
 	"github.com/dasider41/cfootsell/market"
 	"github.com/dasider41/cfootsell/models"
 	"github.com/dasider41/cfootsell/util"
+	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron/v3"
 
 	"github.com/joho/godotenv"
 )
@@ -12,13 +13,17 @@ import (
 func main() {
 	err := godotenv.Load()
 	util.ErrCheck(err)
-	list, err := models.GetConditionList()
-	util.ErrCheck(err)
 
-	for _, v := range list {
-		// fmt.Printf("%v\n", v)
-		market.Update(v)
+	c := cron.New()
+	c.AddFunc("@hourly", func() { market.Scrap() })
+	c.Start()
+
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/market", models.FetchAllProduct)
+		v1.GET("/conditions", models.FetchAllConditions)
 	}
 
-	email.Notification()
+	r.Run()
 }
